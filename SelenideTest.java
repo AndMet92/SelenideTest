5d5c9ba7f748f048f4b25cc987c6a4e11ccda0d2
AndMet92 <andaleme555@gmail.com>
2021-05-03 11:53:59 +0300

    New

---
+++ SelenideTest.java
@@ -0,0 +1,29 @@
+package selenide;
+
+import com.codeborne.selenide.Condition;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.CollectionCondition.exactTexts;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideTest {
+    @Test
+    public void selenide(){
+        open("https://todomvc.com/examples/emberjs");
+        element(By.id("new-todo")).setValue("a").pressEnter();
+        element(By.id("new-todo")).setValue("b").pressEnter();
+        element(By.id("new-todo")).setValue("c").pressEnter();
+        elements("#todo-list>li").shouldHave(exactTexts("a","b","c"));
+        elements("#todo-list>li").findBy(Condition.exactText("b")).find(".toggle").click();
+        elements("#todo-list>li").filterBy(Condition.cssClass("completed")).shouldHave(exactTexts("b"));
+        elements("#todo-list>li").filterBy(Condition.not(Condition.cssClass("completed"))).shouldHave(exactTexts("a","c"));
+
+
+    }
+
+    private void open(java.lang.String string) {
+    }
+
+
+}
---
+++ SelenideTestCss.java
@@ -0,0 +1,25 @@
+package selenide;
+
+import org.junit.Test;
+
+
+    import com.codeborne.selenide.Condition;
+    import org.junit.Test;
+
+    import static com.codeborne.selenide.CollectionCondition.exactTexts;
+    import static com.codeborne.selenide.Selenide.*;
+    public class SelenideTestCss {
+    @Test
+    public void selenide(){
+        open("https://todomvc.com/examples/emberjs");
+
+        element("#new-todo").setValue("a").pressEnter();
+        element("#new-todo").setValue("b").pressEnter();
+        element("#new-todo").setValue("c").pressEnter();
+        elements("#todo-list li").shouldHave(exactTexts("a","b","c"));
+
+        element("#todo-list li:nth-child(2) .toggle").click();
+        elements("#todo-list li.completed").shouldHave(exactTexts("b"));
+        elements("#todo-list li:not(.completed)").shouldHave(exactTexts("a", "c"));
+    }
+}
